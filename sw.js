a3ac1bc6407976d93c0e5062ce420a435d97aa64
emanuelizuchukwu0-oss <emanuelizuchukwu0@gmail.com>
2026-09-21 21:31:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch', () => {});
+// sw.js - Minimal service worker for PWA installability
+self.addEventListener('fetch', () => {
+  // This can be empty. The handler just needs to exist
+  // for Chrome on Android to recognize the app as installable.
+});
